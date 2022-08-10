3324a683c43dea6b43e0957049abe67d76c67443
Fabio Kishino <fabiokishino@hotmail.com>
2022-08-10 16:35:17 -0300

    sign in page layout created

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,27 @@
-import '../styles/pages/SignIn.css';
+import { Link } from 'react-router-dom'
+
+import TakeLogo from '../images/TakeLogo.svg'
+import '../styles/pages/signin.css';
 
 export function SignIn () {
   return (
     <div id="page-signin">
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
+      <div className="container">
+        <form>
+          <img src={TakeLogo} alt="TakeLogo"/>
+
+          <input type="text" placeholder="Username"/>
+          <input type="password" placeholder="Password"/> 
+
+          <div className="buttons">
+            <Link to="/home" className="login-button">Log In</Link>
+            <Link to="/signup" className="signup-button">Sign Up</Link>
+          </div>
+          
+          <Link to="/" className="forget-password">Forget your password?</Link>
+          
+        </form>
+      </div>
     </div>
   )
 }
