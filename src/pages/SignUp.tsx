1249b4431e36cda78b0c76f8ceef2342b5879ec2
Fabio Kishino <fabiokishino@hotmail.com>
2022-08-15 14:19:24 -0300

    add select input

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom'
 import TakeLogo from '../images/TakeLogo.svg'
 import '../styles/pages/signup.css';
 
+import Select from 'react-select'
+
+
+const cityOptions = [
+  { value: '1', label: 'Curitiba' },
+  { value: '2', label: 'SJP' },
+]
+
 export function SignUp () {
   return (
     <div id="page-signup">
@@ -16,7 +24,12 @@ export function SignUp () {
           <input type="password" placeholder="Admin Password"/> 
           <input type="text" placeholder="State"/>
           <input type="text" placeholder="City"/>
-
+          
+          <Select
+            className="select-city"
+            placeholder="City"
+            options={cityOptions}
+          />
 
           <div className="buttons">
             <Link to="/" className="cancel-button">Cancel</Link>
